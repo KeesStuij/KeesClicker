506952abf10c72815bb3286ba004f45977e8f885
KeesStuij <ca.stuij1@student.avans.nl>
2022-02-04 11:08:32 +0100

    Initial Commit

---
+++ KeesClicker/KeesClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeesClickerApp.swift
+//  KeesClicker
+//
+//  Created by Kees Stuij on 04/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct KeesClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
